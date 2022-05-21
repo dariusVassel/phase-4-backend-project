f5e9dc636de43e0ac8c8f873999831555bcfc7f1
+ <+>
2022-05-20 23:57:12 -0400

    update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[ show update destroy ]
-  skip_before_action :authorized, only: [:index, :create]
+  skip_before_action :authorized, only: [ :create]
   # GET /users
   def index
     @users = User.all
