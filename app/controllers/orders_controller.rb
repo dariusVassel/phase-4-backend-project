9a524370d4133659373aaeac2c64f53f4f1302e4
+ <+>
2022-06-22 18:22:10 -0400

    updated orders for nested conditional rendering

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,9 +3,13 @@ class OrdersController < ApplicationController
 
   # GET /orders
   def index
-    @orders = Order.where(user: current_user).order(PO_date: :desc)
-
-    render json: @orders, include: [:product, :contact]
+    if params[:product_id]
+      product= Product.find_by(id: params[:product_id])
+      @orders= product.orders.where(user: current_user)
+    else
+      @orders = Order.where(user: current_user).order(PO_date: :desc)
+    end
+      render json: @orders, include: [:product, :contact]
   end
 
   
