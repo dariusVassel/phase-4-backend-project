c9fe7dfc7491f5743e7244d85b4ca7924bb84570
+ <+>
2022-06-22 20:21:54 -0400

    update serializer order attributes

--- app/serializers/order_serializer.rb
+++ app/serializers/order_serializer.rb
@@ -1,3 +1,4 @@
 class OrderSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :PO, :cases, :kilos, :price_kg, :price_total, :product_id, :contact_id, :user_id, :payment_id, :packing_id, :port_from,
+  :port_to, :selected_size, :selected_freeze, :selected_glaze, :catching_method, :shipment_date, :PO_date, :order_ref_number, :order_status, :total_kilos, :specifications
 end
