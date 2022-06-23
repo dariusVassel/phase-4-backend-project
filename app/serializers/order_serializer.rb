fce2f3e4c5e443430614a18a469115384ddb8b57
+ <+>
2022-06-22 21:01:37 -0400

    connecting many-to-many relationships

--- app/serializers/order_serializer.rb
+++ app/serializers/order_serializer.rb
@@ -1,4 +1,7 @@
 class OrderSerializer < ActiveModel::Serializer
-  attributes :PO, :cases, :kilos, :price_kg, :price_total, :product_id, :contact_id, :user_id, :payment_id, :packing_id, :port_from,
+  attributes :id, :PO, :cases, :kilos, :price_kg, :price_total, :product_id, :contact_id, :user_id, :payment_id, :packing_id, :port_from,
   :port_to, :selected_size, :selected_freeze, :selected_glaze, :catching_method, :shipment_date, :PO_date, :order_ref_number, :order_status, :total_kilos, :specifications
+
+  belongs_to :product
+  belongs_to :contact
 end
